35db34823cf0a7ea3e0f083d1f65066d2ad7d2b8
modhtom <85494929+modhtom@users.noreply.github.com>
2023-03-07 02:32:48 +0200

    Adding fizz buzz.cpp

---
+++ fizz buzz/fizz buzz/fizz buzz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 1; i <= 100; i++) {
+		if (i%3==0&& i % 5 == 0) {
+			cout << "fizz buzz" << " ";
+		}
+		else if (i % 3 == 0) {
+			cout << "fizz" << " ";
+		}
+		else if (i % 5 == 0) {
+			cout << "buzz" << " ";
+		}
+		else {
+			cout << i<<" ";
+		}
+	}
+	return 0;
+}
