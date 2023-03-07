27e95304875b2b872f103706df5027652c56b6dc
modhtom <85494929+modhtom@users.noreply.github.com>
2023-03-07 02:51:55 +0200

    Adding Theatre Square.cpp

---
+++ Theatre Square/Theatre Square/Theatre Square.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main(void)
+{
+    double n, m, a;
+    scanf("%lf %lf %lf", &n, &m, &a);
+    printf("%1.lf\n", ceil(n / a) * ceil(m / a));
+    return 0;
+}
